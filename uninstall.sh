dd49a5d279e59b9b8af295ad3f1a4a4e6e422396
Joel Haker <joeladam@gmail.com>
2020-08-06 19:31:27 -0400

    remove stuff from uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-## Variables
-CWD=$(pwd)
-
-# First install messages in color
-if [ -d "/usr/local/bin" ] && [ -e "/usr/local/bin/cmsg" ]; then
-    rm /usr/local/bin/cmsg
-fi
-
 # TODO: uninstall the rest of the dotfiles

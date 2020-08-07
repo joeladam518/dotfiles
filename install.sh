0cf17568b0150bd8cd68e95adbca5b8ad8aaa0b9
Joel Haker <joeladam@gmail.com>
2020-08-06 23:14:59 -0400

    add install script

--- install.sh
+++ install.sh
@@ -1,49 +1,62 @@
 #!/usr/bin/env bash
 
-## Variables
-CWD=$(pwd)
-BIN_DIR="$HOME/bin"
-REPOS_DIR="$HOME/repos"
-DOTFILES_DIR="$REPOS_DIR/dotfiles"
+# Variables
+CWD="$(pwd)"
+script_dir="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
+bin_dir="${script_dir}/bin"
+repos_dir="${HOME}/repos"
+system="$1"
 
-if [ ! -d "$REPOS_DIR" ]; then
-    cd "$HOME" && mkdir "$REPOS_DIR"
+# Add the bin dir to path if it's not there
+if [[ ! "$PATH" =~ (^|:)"$bin_dir"(:|$) ]]; then
+    export PATH="${PATH}:${bin_dir}"
 fi
 
-if [ ! -d "$DOTFILES_DIR" ]; then
-    cd "$REPOS_DIR" && git clone git@github.com:joeladam518/dotfiles.git
-fi
-
-if [ ! -d "$BIN_DIR" ]; then
-    cd "$HOME" && mkdir "$BIN_DIR"
-    PATH="$BIN_DIR:$PATH"
-fi
-
-# First install messages in color
-if [ ! -f "$DOTFILES_DIR/bin/cmsg" ]; then
-    echo "Could not find the cmsg program to install..."
+# Check if the system argument was given
+if [ -z "$system" ] || { [ "$system" != "desktop" ] && [ "$system" != "mac" ] && [ "$system" != "server" ]; }; then
+    cmsg -an "Must specify a type of system. "
+    cmsg -a  "Valid types: {desktop|mac|server}"
     exit 1
 fi
 
-if [ ! -e "$BIN_DIR/cmsg" ]; then
-    cd "$BIN_DIR" && ln -sf "$DOTFILES_DIR/bin/cmsg"
+# Create the repos directory if it doesn't exist
+if [ ! -d "$repos_dir" ]; then
+    mkdir -p "$repos_dir"
 fi
 
-# Install .bashrc files
-if [ -f "$HOME/.bashrc" ] && [ ! -f "$HOME/.bashrc.old" ]; then
-    cd "$HOME" && mv "$HOME/.bashrc" "$HOME/.bashrc.old"
-    cd "$HOME" && ln -sf "$DOTFILES_DIR/bashrc/desktop/.bashrc"
-    source "$HOME/.bashrc"
+# Set the rcfile variable (because mac is different from linux)
+if [ "$system" == "mac" ]; then
+    rcfile=".bash_profile"
+else
+    rcfile=".bashrc"
 fi
 
-# Install vim config files
-if [ ! -L "$HOME/.vim" ] && [ ! -d "$HOME/.vim"]; then
-    cd "$HOME" && ln -sf "$DOTFILES_DIR/vimrc/.vim"
+# Symlink the bashrc files
+if [ -L "${HOME}/${rcfile}" ] && [ -f "${HOME}/${rcfile}.old" ]; then
+    cmsg -y "The dotfiles .bashrc symlink already exists"
+elif [ -f "${HOME}/${rcfile}" ]; then
+    cd "$HOME" && mv "$rcfile" "${rcfile}.old"
+    cd "$HOME" && ln -s "${repos_dir}/dotfiles/bashrc/${system}/${rcfile}" .
 fi
 
-if [ ! -L "$HOME/.vimrc" ] && [ ! -f "$HOME/.vimrc"]; then
-    cd "$HOME" && ln -sf "$DOTFILES_DIR/vimrc/.vimrc"
+# Symlink the vimrc files
+if [ -L "${HOME}/.vimrc" ] && { [ -L "${HOME}/.vim" ] && [ -d "${HOME}/.vim" ]; }; then
+    cmsg -y "The dotfiles .vimrc symlinks already exist"
+else
+    cd "$HOME" && ln -s "${repos_dir}/dotfiles/vimrc/.vim" .
+    cd "$HOME" && ln -s "${repos_dir}/dotfiles/vimrc/.vimrc" .
 fi
 
-# TODO: install the rest of the dotfiles
+# TODO: intall the rest of the dotfiles
+
+# Add Tmux config
+cd "$HOME" && ln -s "${repos_dir}/dotfiles/tmux/.tmux.conf.tmp2 .tmux.conf"
+
+# Add git config 
+if [ "$system" == "desktop"] && [ "$system" == "mac"]; then
+    cd "$HOME" && ln -s "${repos_dir}/dotfiles/git/.gitconfig"
+fi
+
+cd "$CWD" || exit
+exit
 
